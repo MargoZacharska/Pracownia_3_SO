81426590f5cbf489698e1ac865268577dc4f1c5c
MargoZacharska <malgorzata-zacharska@wp.pl>
2018-01-21 23:10:50 +0100

    Poprawione bledy

--- Makefile
+++ Makefile
@@ -4,5 +4,5 @@ program : main.cpp
 	g++ -std=c++11 -pthread -Wall -Wextra -Werror main.cpp -o program
 	
 run :
-	./program 5 10 5 10
+	./program 10 10 5 10
 
--- main.cpp
+++ main.cpp
@@ -7,7 +7,9 @@
 #include "unistd.h"
 #include <vector>
 
+
 using namespace std;
+
 
 #define KUCHARZ "kucharz"
 #define MYSLIWY "mysliwy"
@@ -37,6 +39,7 @@ public:
 	bool TakeOneIfExists();
 };
 
+
 struct DaneWatku{
 	bool (*akcja)();
 	bool zyje;
@@ -49,6 +52,7 @@ ConcurrentResource::~ConcurrentResource()
 	pthread_mutex_destroy(&mutex);
 }
 
+
 int roll_K6_dice()
 {
 	int x;
@@ -56,12 +60,14 @@ int roll_K6_dice()
 	return x;
 }
 
+
 void ConcurrentResource::add(int x)
 {
 	pthread_mutex_lock(&mutex);
 	resource += x;
 	pthread_mutex_unlock(&mutex);
 }
+
 
 int ConcurrentResource::get()
 {
@@ -87,11 +93,11 @@ bool ConcurrentResource::TakeOneIfExists()
 }
 
 
-
 ConcurrentResource jedzenie;
 ConcurrentResource zwierzyna;
 ConcurrentResource liczbaAktorow;
 ConcurrentResource polany[POLANY];
+
 
 sem_t poczatekNocy;
 sem_t poczatekDnia;
@@ -103,7 +109,7 @@ bool watekMysliwego(){
 
 	int polana = rand() % POLANY;
 	if (polany[polana].TakeOneIfExists()){
-		if (hunterLack > animalLack + 3){
+		if (hunterLack > animalLack){
 			zwierzyna.add(1);
 		}
 	}
@@ -112,6 +118,7 @@ bool watekMysliwego(){
 	}
 	return jedzenie.TakeOneIfExists();
 }
+
 
 void *watekOgolny(void *_dane){
 	DaneWatku *dane = (DaneWatku*)_dane;
@@ -130,6 +137,7 @@ void *watekOgolny(void *_dane){
 	return NULL;
 }
 
+
 bool watekKucharza(){
 	if (zwierzyna.TakeOneIfExists()){
 		int n = roll_K6_dice();
@@ -137,6 +145,7 @@ bool watekKucharza(){
 	}
 	return jedzenie.TakeOneIfExists();
 }
+
 
 void dodajAktora(vector<std::pair<pthread_t*, DaneWatku*> > &vec, bool akcja(), string rola){
 
@@ -170,48 +179,60 @@ int main(int argc, char *argv[])
 {
 	srand(time(NULL));
 
+
 	if (argc<5){
 		return 0;
 	}
+	
+	
 	int liczba_mysliwych = atoi(argv[1]);
 	int liczba_kucharzy = atoi(argv[2]);
 	int liczba_zwierzyny = atoi(argv[3]);
 	int liczba_jedzenia = atoi(argv[4]);
 
+
 	int calkowita_liczba_watkow = liczba_kucharzy + liczba_mysliwych;
 	liczbaAktorow.resource = calkowita_liczba_watkow;
+
 
 	sem_init(&poczatekDnia, 0, 0);
 	sem_init(&poczatekNocy, 0, 0);
 
+
 	vector<std::pair<pthread_t*, DaneWatku*> > aktorzy;
+
 
 	for (int j = 0; j < liczba_mysliwych; j++){
 		dodajAktora(aktorzy, watekMysliwego, MYSLIWY);
 	}
 
+
 	for (int j = 0; j < liczba_kucharzy; j++){
 		dodajAktora(aktorzy, watekKucharza, KUCHARZ);
 	}
 
+
 	jedzenie.add(liczba_jedzenia);
 	zwierzyna.add(liczba_zwierzyny);
 
+
 	int calkowita_liczba_watkow_tmp;
 
-	for (int i = 0; i < 5; i++)
+
+	for (int i = 0; i < 25; i++)
 	{
 		int tmp = 0;
+
 
 		for (int i = 0; i < calkowita_liczba_watkow; i++){
 			sem_post(&poczatekNocy);
 		}
 
+
 		do{
 			sem_getvalue(&poczatekNocy, &tmp);
 			usleep(500*1000);
 		} while (tmp != 0);
-
 
 
 		calkowita_liczba_watkow_tmp = liczbaAktorow.resource;
@@ -223,22 +244,26 @@ int main(int argc, char *argv[])
 			<< "\nmysliwych: \t" << policzWystapienia(aktorzy, MYSLIWY)
 			<< "\nzwierzyny: \t" << zwierzyna.resource
 			<< "\njedzenia:  \t" << jedzenie.resource;
-			if (calkowita_liczba_watkow == 0)
+			if (calkowita_liczba_watkow_tmp == 0)
 			{
-				cout << "\nOsada wyginela ";
+				cout << "\nOsada wyginela\n";
+				break;
 			}
 			cout  << "\n-------------------------------------\n";
 
-		//calkowita_liczba_watkow_tmp = liczbaAktorow.resource;
+
 		for (int i = 0; i < calkowita_liczba_watkow; i++){
 			sem_post(&poczatekDnia);
 		}
 
+
 		calkowita_liczba_watkow = calkowita_liczba_watkow_tmp;
+		
 		
 		for (int i = 0; i < ZWIERZYNA; i++){
 			polany[rand() % POLANY].resource++;
 		}
+
 
 		do{
 			sem_getvalue(&poczatekDnia, &tmp);
